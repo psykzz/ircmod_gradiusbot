2f3a7bf93297150240f4d347a300b255475aa116
gradiuscypher <gradiuscypher@gmail.com>
2013-12-01 21:15:11 -0800

    changing function filler actions, accidently got into privmessage loop when testing

--- mod_functions.py
+++ mod_functions.py
@@ -6,20 +6,20 @@ class ModFunctions():
         print
 
     def admin(self, event, message_callback_function):
-        message_callback_function(event.target, "Command was " + event.message.split("@")[1])
+        print
 
     def commands(self, event, message_callback_function):
-        message_callback_function(event.target, "Command was " + event.message.split("!")[1])
+        print
 
     def flag_words(self, event, message_callback_function):
-        message_callback_function(event.target, event.source + " said " + event.message)
+        print
 
     def flag_urls(self, event, message_callback_function):
         bad_urls = []
-        message_callback_function(event.target, event.source + " said " + event.message)
+        print
 
     def check_for_spam(self, event, message_callback_function):
-        message_callback_function(event.target, event.source + " said " + event.message)
+        print
 
     def stats_tracker(self, event):
         print "STATS"
